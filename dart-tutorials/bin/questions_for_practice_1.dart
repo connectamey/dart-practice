7cc395024f7365429a99df03a4483148e43bb7a0
Amey Bansod <ameygotemail@gmail.com>
2023-07-04 02:46:41 -0400

    9 Questions done. 4 remaining from set 1

--- dart-tutorials/bin/questions_for_practice_1.dart
+++ dart-tutorials/bin/questions_for_practice_1.dart
@@ -13,10 +13,36 @@ Write a dart program to convert String to int.
 Suppose, you often go to restaurant with friends and you have to split amount of bill. Write a program to calculate split amount of bill. Formula= (total bill amount) / number of people
 */
 
-import 'package:dart_tutorials/functions_questions_for_practice_1.dart' as functions_questions_for_practice_1;
+import 'package:dart_tutorials/functions_questions_for_practice_1.dart'
+    as functions_questions_for_practice_1;
 
 void main(List<String> arguments) {
-  print('Write a program to print your name in Dart.');
-  print (functions_questions_for_practice_1.printName());
+  print('Question:\nWrite a program to print your name in Dart.');
+  print('Answer:\n${functions_questions_for_practice_1.one_PrintName()}');
 
+  print(
+      'Question:\nWrite a program to print Hello I am “John Doe” and Hello I’am “John Doe” with single and double quotes.');
+  print('Answer:\n${functions_questions_for_practice_1.two_PrintSentence()}');
+
+  print('Question:\nDeclare constant type** of int set value 7.');
+  print('Answer:\n${functions_questions_for_practice_1.three_ConstantType()}');
+
+  print(
+      'Question:\nWrite a program in Dart that finds simple interest. Formula= (p * t * r) / 100');
+  print(
+      'Answer:\nSimple interest is principal * time * rate of interest / 100 = ${functions_questions_for_practice_1.four_SimpleInterest()}');
+
+  print(
+      'Question:\nWrite a program to print a square of a number using user input.');
+  print(
+      'Answer:\n The square is ${functions_questions_for_practice_1.five_square()}');
+
+  print(
+      'Question:\nWrite a program to print full name of a from first name and last name using user input.');
+  print(
+      'Answer:\n${functions_questions_for_practice_1.six_FirstName_LastName()}');
+
+  print('Write a program to find quotient and remainder of two integers.');
+  print(
+      'Answer:\n${functions_questions_for_practice_1.seven_Quotient_Remainder()}');
 }
--- dart-tutorials/lib/functions_questions_for_practice_1.dart
+++ dart-tutorials/lib/functions_questions_for_practice_1.dart
@@ -1,5 +1,55 @@
-String printName()
-{
+import 'dart:io';
+import 'package:io/io.dart';
+
+String one_PrintName() {
   String name = "Amey Bansod";
   return name;
+}
+
+String two_PrintSentence() {
+  String doubleQuoteString1 = "Hello I am “John Doe”";
+  String doubleQuoteString2 = "I’am “John Doe”";
+  return "$doubleQuoteString1 $doubleQuoteString2";
+}
+
+int three_ConstantType() {
+  const value = 7;
+  return value;
+}
+
+num four_SimpleInterest() {
+  print('Enter principal amount');
+  String? principal = stdin.readLineSync();
+  print('Enter time');
+  String? time = stdin.readLineSync();
+  print('Enter rate of interest');
+  String? rateOfInterest = stdin.readLineSync();
+  return (num.parse(principal!) *
+      (num.parse(time!)) *
+      (num.parse(rateOfInterest!)) /
+      100);
+}
+
+num five_square() {
+  print('Enter number to calculate square ');
+  String? numberToSquare = stdin.readLineSync();
+  return (num.parse(numberToSquare!) * num.parse(numberToSquare));
+}
+
+String six_FirstName_LastName() {
+  print('Enter first name');
+  String? firstName = stdin.readLineSync();
+  print('Enter last name');
+  String? lastName = stdin.readLineSync();
+  return ('Full name is $firstName $lastName');
+}
+
+String seven_Quotient_Remainder() {
+  print('Enter first integer');
+  String? firstInteger = stdin.readLineSync();
+  print('Enter second integer');
+  String? secondInteger = stdin.readLineSync();
+  num quotient = num.parse(firstInteger!) ~/ num.parse(secondInteger!);
+  num remainder = num.parse(firstInteger).remainder(num.parse(secondInteger));
+  return ('Quotient is $quotient and remainder is $remainder');
 }
